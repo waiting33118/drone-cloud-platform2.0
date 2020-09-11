ce601fe0717edb6a7e4dc2b92d43dbb3e0d04c75
TonyChung <waiting33118@gmail.com>
2020-09-11 20:51:36 +0800

    feat: create client view page

---
+++ src/views/Client.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button class="btn btn-primary rounded-pill" @click.prevent.stop="getPermission">請求權限</button>
+    <video autoplay></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Client',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    // async featureDetection () {
+    //   console.log(navigator.mediaDevices)
+    //   console.log(navigator.mediaDevices.getUserMedia)
+    //   const checkSupported = await !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+    //   checkSupported ? this.getPermission() : alert('此瀏覽器無支援userMedia功能')
+    // },
+    async getPermission () {
+      console.log('getPermission')
+      const constraints = {
+        video: true,
+        audio: true
+      }
+      const video = document.querySelector('video')
+      await navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => { video.srcObject = stream })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
