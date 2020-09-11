540c079ca7ce2bb49999082c7b3663079a539128
TonyChung <waiting33118@gmail.com>
2020-09-11 17:25:23 +0800

    feat: edit deploy config & script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/waiting33118/vue-mapboxgl.git master:gh-pages
+git push -f https://github.com/waiting33118/drone-cloud-platform2.0.git master:gh-pages
 
 cd -
