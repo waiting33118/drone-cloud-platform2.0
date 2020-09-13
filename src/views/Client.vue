2873e236d02273047a17c140afdfc289d4d8594a
Tony <waiting33118@gmail.com>
2020-09-13 11:44:38 +0800

    feat: edit methods

--- src/views/Client.vue
+++ src/views/Client.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <button class="btn btn-primary rounded-pill" @click.prevent.stop="getPermission">請求權限</button>
-    <video autoplay></video>
+  <div id="wapper">
+    <video autoplay controls width="500"></video>
   </div>
 </template>
 
@@ -10,28 +9,54 @@ export default {
   name: 'Client',
   data () {
     return {
+      constraints: {
+        video: { width: { exact: 1280 }, height: { exact: 720 } },
+        audio: false
+      }
     }
   },
   methods: {
-    // async featureDetection () {
-    //   console.log(navigator.mediaDevices)
-    //   console.log(navigator.mediaDevices.getUserMedia)
-    //   const checkSupported = await !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
-    //   checkSupported ? this.getPermission() : alert('此瀏覽器無支援userMedia功能')
-    // },
-    async getPermission () {
-      console.log('getPermission')
-      const constraints = {
-        video: true,
-        audio: true
+    async featureDetection () {
+      try {
+        console.log(navigator.mediaDevices.getSupportedConstraints())
+        const checkSupported = await !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+        checkSupported ? this.getPermission() : alert('此瀏覽器無支援userMedia功能')
+      } catch (error) {
+        throw new Error(error)
       }
-      const video = document.querySelector('video')
-      await navigator.mediaDevices.getUserMedia(constraints)
-        .then(stream => { video.srcObject = stream })
+    },
+    async getPermission () {
+      try {
+        const video = document.querySelector('video')
+        await navigator.mediaDevices.getUserMedia(this.constraints)
+          .then(stream => {
+            const videoTracks = stream.getVideoTracks()
+            const audioTracks = stream.getAudioTracks()
+            video.srcObject = stream
+            window.stream = stream
+            console.log(videoTracks)
+            console.log(audioTracks)
+          })
+      } catch (error) {
+        throw new Error(error)
+      }
     }
+  },
+  created () {
+    this.featureDetection()
   }
 }
 </script>
 
 <style scoped>
+  #wapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  video {
+    display: block;
+    box-shadow: 0px 0px 8px 6px gray;
+    border-radius: 5px;
+  }
 </style>
