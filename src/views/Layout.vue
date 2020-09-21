098c641bf28ca3cfb8b39e95edd8c9a87de9ea1f
TonyChung <waiting33118@gmail.com>
2020-09-21 19:51:54 +0800

    feat: add map resize listner

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -11,11 +11,6 @@ import './../../node_modules/golden-layout/src/css/goldenlayout-light-theme.css'
 
 export default {
   name: 'Layout',
-  data () {
-    return {
-      accessToken: 'pk.eyJ1Ijoid2FpdGluZzMzMTE4IiwiYSI6ImNrZDVlZWp6MjFxcXQyeHF2bW0xenU4YXoifQ.iGfojLdouAjsovJuRxjYVA'
-    }
-  },
   mounted () {
     const config = {
       content: [
@@ -58,89 +53,91 @@ export default {
     })
     myLayout.registerComponent('mapComponent', function (container, state) {
       container.getElement()[0].id = 'mapbox'
+      container.on('open', () => {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoid2FpdGluZzMzMTE4IiwiYSI6ImNrZDVlZWp6MjFxcXQyeHF2bW0xenU4YXoifQ.iGfojLdouAjsovJuRxjYVA'
+        const map = new mapboxgl.Map({
+          style: 'mapbox://styles/waiting33118/ckdfkx3t10k9w1irkp8anuy39',
+          center: { lon: 121.534907, lat: 25.043163 },
+          zoom: 17,
+          pitch: 55,
+          bearing: 0,
+          antialias: true,
+          container: 'mapbox'
+        })
+
+        container.on('resize', () => {
+          map.resize()
+        })
+
+        const nav = new mapboxgl.NavigationControl()
+        map.addControl(nav, 'top-right')
+
+        const geolocate = new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true
+        })
+        map.addControl(geolocate, 'top-right')
+        map.addControl(new mapboxgl.FullscreenControl(), 'top-left')
+
+        // The 'building' layer in the mapbox-streets vector source contains building-height data from OpenStreetMap.
+        map.on('load', () => {
+          // Insert the layer beneath any symbol layer.
+          const layers = map.getStyle().layers
+          let labelLayerId
+          for (let i = 0; i < layers.length; i++) {
+            if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+              labelLayerId = layers[i].id
+              break
+            }
+          }
+          map.addLayer(
+            {
+              id: '3d-buildings',
+              source: 'composite',
+              'source-layer': 'building',
+              filter: ['==', 'extrude', 'true'],
+              type: 'fill-extrusion',
+              minzoom: 17,
+              paint: {
+                'fill-extrusion-color': '#aaa',
+                // use an 'interpolate' expression to add a smooth transition effect to the buildings as the user zooms in
+                'fill-extrusion-height': [
+                  'interpolate',
+                  ['linear'],
+                  ['zoom'],
+                  15,
+                  0,
+                  15.05,
+                  ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                  'interpolate',
+                  ['linear'],
+                  ['zoom'],
+                  15,
+                  0,
+                  15.05,
+                  ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+              }
+            },
+            labelLayerId
+          )
+        })
+
+        const scale = new mapboxgl.ScaleControl({
+          maxWidth: 80,
+          unit: 'imperial'
+        })
+        map.addControl(scale)
+        scale.setUnit('metric')
+      })
     })
     myLayout.init()
     window.addEventListener('resize', () => myLayout.updateSize())
-
-    /* mapbox */
-    setTimeout(() => {
-      mapboxgl.accessToken = this.accessToken
-      const map = new mapboxgl.Map({
-        style: 'mapbox://styles/waiting33118/ckdfkx3t10k9w1irkp8anuy39',
-        center: { lon: 121.534907, lat: 25.043163 },
-        zoom: 17,
-        pitch: 55,
-        bearing: 0,
-        antialias: true,
-        container: 'mapbox'
-      })
-
-      const nav = new mapboxgl.NavigationControl()
-      map.addControl(nav, 'top-right')
-
-      const geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        trackUserLocation: true
-      })
-      map.addControl(geolocate, 'top-right')
-      map.addControl(new mapboxgl.FullscreenControl(), 'top-left')
-
-      // The 'building' layer in the mapbox-streets vector source contains building-height data from OpenStreetMap.
-      map.on('load', () => {
-      // Insert the layer beneath any symbol layer.
-        const layers = map.getStyle().layers
-        let labelLayerId
-        for (let i = 0; i < layers.length; i++) {
-          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-            labelLayerId = layers[i].id
-            break
-          }
-        }
-        map.addLayer(
-          {
-            id: '3d-buildings',
-            source: 'composite',
-            'source-layer': 'building',
-            filter: ['==', 'extrude', 'true'],
-            type: 'fill-extrusion',
-            minzoom: 17,
-            paint: {
-              'fill-extrusion-color': '#aaa',
-              // use an 'interpolate' expression to add a smooth transition effect to the buildings as the user zooms in
-              'fill-extrusion-height': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                15,
-                0,
-                15.05,
-                ['get', 'height']
-              ],
-              'fill-extrusion-base': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                15,
-                0,
-                15.05,
-                ['get', 'min_height']
-              ],
-              'fill-extrusion-opacity': 0.6
-            }
-          },
-          labelLayerId
-        )
-      })
-
-      const scale = new mapboxgl.ScaleControl({
-        maxWidth: 80,
-        unit: 'imperial'
-      })
-      map.addControl(scale)
-      scale.setUnit('metric')
-    }, 10)
   }
 }
 </script>
